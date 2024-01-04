62e70ee92778bfc191deabc9a2a70c59aedc8de5
AnaClaraCS <anaclaracostasouza@yahoo.com.br>
2024-01-04 10:43:39 -0300

    autenticação do usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
+  root            to: 'static_pages#index'
+  get 'sobre',    to: 'static_pages#sobre'
+  get 'contato',  to: 'static_pages#contato'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'cadastro', to: 'users#new'
+  get 'entrar',   to: 'sessions#new'
+  post 'entrar',  to: 'sessions#create'
+  delete 'sair',  to: 'sessions#destroy'
 
-  root to: "static_pages#index"
-
-  get 'sobre', to: 'static_pages#sobre'
-  get 'contato', to: 'static_pages#contato'
-
-  resources :contacts
-  resources:users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create, :edit, :update] do
+    resources :contacts, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+  resources :sessions, only: [:new, :create, :destroy]
 end
